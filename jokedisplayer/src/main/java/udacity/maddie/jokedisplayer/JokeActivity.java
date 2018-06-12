3c17aa03c93eae638407bb469100985eb63857cc
Beyl, Madeline <Madeline.Beyl@capitalone.com>
2018-06-11 21:00:58 -0700

    set up project

---
+++ jokedisplayer/src/main/java/udacity/maddie/jokedisplayer/JokeActivity.java
@@ -0,0 +1,28 @@
+package udacity.maddie.jokedisplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class JokeActivity extends Activity {
+
+    public static final String JOKE_ID = "joke";
+
+    private String joke;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        joke = getIntent().getStringExtra(JOKE_ID);
+
+        TextView jokeTextView = findViewById(R.id.joke_text_view);
+        jokeTextView.setText(joke);
+    }
+}
---
+++ jokelib/src/main/java/udacity/maddie/jokelib/JokeTeller.java
@@ -0,0 +1,11 @@
+package udacity.maddie.jokelib;
+
+public class JokeTeller {
+
+    public static String getJoke() {
+        return "omg hahahhaa";
+    }
+
+}
+
+
