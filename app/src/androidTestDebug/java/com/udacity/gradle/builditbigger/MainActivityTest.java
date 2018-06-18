915ab7877d7c363047b5751627f6ee8fa0eb421b
Beyl, Madeline <Madeline.Beyl@capitalone.com>
2018-06-18 13:19:11 -0700

    add main activity test, resolve dependency issues

--- app/src/androidTestDebug/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTestDebug/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -8,11 +9,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import udacity.maddie.jokedisplayer.JokeActivity;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest  {
@@ -29,7 +32,10 @@ public class MainActivityTest  {
 
     @Test
     public void retrieveJoke(){
+        Intents.init();
         onView(withId(R.id.tell_joke_button)).perform(click());
+        intended(hasComponent(JokeActivity.class.getName()));
+        Intents.release();
     }
 
 }
