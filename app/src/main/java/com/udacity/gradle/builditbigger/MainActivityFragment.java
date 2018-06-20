6fa25dbcc479bee145cdc3b29c1a419e3c53fec8
Beyl, Madeline <Madeline.Beyl@capitalone.com>
2018-06-19 20:35:57 -0700

    remove unused imports, set ad dependency to be only free

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,18 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
-import udacity.maddie.jokedisplayer.JokeActivity;
-import udacity.maddie.jokelib.JokeTeller;
 
 
 /**
